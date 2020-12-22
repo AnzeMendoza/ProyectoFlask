074525e1ffea8ca4f7a6ca504a7a1e5249a37b23
anze.mendoza <anze.mendoza@gmail.om>
2020-12-22 13:22:08 -0300

    primer commit

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+todos = ['todo 1', 'todo 2 ', 'todo 3']
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    responce = make_response(redirect('/hello'))
+    responce.set_cookie('user_ip', user_ip)
+
+    return responce
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip':user_ip,
+        'todos':todos
+    }
+    return render_template('hello.html', **context)
